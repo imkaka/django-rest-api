514f9f454c271cbb322233e3d4f7181748e642ce
anitri10 <91718883+anitri10@users.noreply.github.com>
2022-10-17 13:06:23 +0530

    Resolve for PYL-W0221 deepsource issue.

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -25,7 +25,7 @@ class Snippet(models.Model):
     owner = models.ForeignKey(
         'auth.User', related_name='snippets', on_delete=models.CASCADE)
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):  # skipcq PYL-W0221
         """
         Use the `pygments` library to create a highlighted HTML
         representation of the code snippet.
